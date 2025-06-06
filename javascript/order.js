d35faaba7a233d575d996ede664590157e1e8139
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-06-06 10:11:31 +0530

    Modified Styling of Item Cart

--- javascript/order.js
+++ javascript/order.js
@@ -7,7 +7,13 @@ if (cart.length === 0) {
   cart.forEach((element) => {
     orderCartContainer.innerHTML += `
     <div class="order-cart-item">
-<p><strong>${element.itemName} - ${element.price}</strong></p>
+            <p class="cart-item-name"><strong>${element.itemName}</strong></p> 
+            <div class="inc-dec-container">
+                <button class="btn-inc-dec" id="minus">-</button>   
+                <p id="quantity">Q</p>   
+                <button class="btn-inc-dec" id="plus">+</button>
+            </div> 
+             <p class="cart-item-price"><strong>${element.price}</strong></p>
     </div>`;
   });
 }
