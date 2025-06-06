b00d5a038e1e4b5989246bb24b082e82c6793e33
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-06-06 15:07:56 +0530

    logic to add and minus the quantity

--- javascript/order.js
+++ javascript/order.js
@@ -9,12 +9,25 @@ if (cart.length === 0) {
     <div class="order-cart-item">
             <p class="cart-item-name"><strong>${element.itemName}</strong></p> 
             <div class="inc-dec-container">
-                <button class="btn-inc-dec" id="minus">-</button>   
-                <p id="quantity">Q</p>   
-                <button class="btn-inc-dec" id="plus">+</button>
+                <button class="btn-inc-dec" id="minus" onclick="dec()">-</button>   
+                <p id="quantity">1</p>   
+                <button class="btn-inc-dec" id="plus" onclick="inc()">+</button>
             </div> 
              <p class="cart-item-price"><strong>${element.price}</strong></p>
              <img src="../images/icons/delete.png" id="delete-icon" alt="Delete Icon"/> 
     </div>`;
   });
+}
+
+let itemQuantity = document.getElementById("quantity");
+
+function inc() {
+  itemQuantity.innerText = parseInt(itemQuantity.innerText) + 1;
+}
+
+function dec() {
+  if (itemQuantity.innerText == 1) {
+    return alert("Quantity cannot be 0");
+  }
+  itemQuantity.innerText = parseInt(itemQuantity.innerText) - 1;
 }
